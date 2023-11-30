7e3a878472c7fd8a1cc59e585e376f044a1f335d
kolchenkoav <kolchenkoav@gmail.com>
2023-12-01 00:16:50 +0300

    Parsing lemma +

--- src/main/java/searchengine/parsing/ParseLemma.java
+++ src/main/java/searchengine/parsing/ParseLemma.java
@@ -5,54 +5,67 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import searchengine.lemma.LemmaFinder;
 import searchengine.lemma.LemmaFinderEn;
-import searchengine.model.Index;
-import searchengine.model.Lemma;
+import searchengine.model.*;
 import searchengine.repository.IndexRepository;
 import searchengine.repository.LemmaRepository;
+import searchengine.repository.PageRepository;
+import searchengine.repository.SiteRepository;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.List;
 
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class ParseLemma {
     private final LemmaRepository lemmaRepository;
     private final IndexRepository indexRepository;
+    private final SiteRepository siteRepository;
+    private final PageRepository pageRepository;
 
     public List<String> parsing(String text) {
-        LemmaFinder lemmaFinder = null;
-        LemmaFinderEn lemmaFinderEn = null;
-        try {
-            lemmaFinder = LemmaFinder.getInstance();
-            lemmaFinderEn = LemmaFinderEn.getInstance();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        LemmaFinder lemmaFinder = null;
+//        LemmaFinderEn lemmaFinderEn = null;
+//        try {
+//            lemmaFinder = LemmaFinder.getInstance();
+//            lemmaFinderEn = LemmaFinderEn.getInstance();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        List<String> listEn = lemmaFinderEn.getLemmaList(text);
+//        listEn.forEach(System.out::println);
+//        System.out.println();
+//        List<String> listRu = lemmaFinder.getLemmaList(text);
+//        listRu.forEach(System.out::println);
+//        System.out.println("============================");
+//        System.out.println();
+//        listEn.addAll(listRu);
+//        listEn.forEach(System.out::println);
 
-        List<String> listEn = lemmaFinderEn.getLemmaList(text);
-        listEn.forEach(System.out::println);
-        System.out.println();
-        List<String> listRu = lemmaFinder.getLemmaList(text);
-        listRu.forEach(System.out::println);
-        System.out.println("============================");
-        System.out.println();
-        listEn.addAll(listRu);
-        listEn.forEach(System.out::println);
+        SiteE siteE = new SiteE(Status.INDEXED, new Timestamp(System.currentTimeMillis()), "qqq", "Qu");
+        siteE = siteRepository.save(siteE);
+        int siteId = siteE.getSiteId();
 
-        int frequency = 0;
+        Page page = new Page(siteId, "path", 200, "context");
+        page = pageRepository.save(page);
+        int pageId = page.getPageId();;
+
+        int frequency = 1;
         String lemmaText = "Хлеб";
-        int siteId = 11;
+
         Lemma lemma = new Lemma(siteId, lemmaText, frequency);
         lemma = lemmaRepository.save(lemma);
         int lemmaId = lemma.getLemmaId();
 
-        int pageId = 1130;
         int rank = 15;
         Index index = new Index(pageId, lemmaId, rank);
         indexRepository.save(index);
 
-        return listEn;
+        return null;
     }
 }
